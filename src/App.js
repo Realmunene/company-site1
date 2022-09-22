54b7fd85be738afe43eb4f6490cf390b17539c17
Realmunene <josep.mwangi@student.moringaschool.com>
2022-09-22 13:49:19 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import insideImage from './img/dafcc0cac604ae3275bb8cb77a96cd68.jpg';
 import './App.css';
 
 function App() {
@@ -5,6 +7,7 @@ function App() {
     <div className="App">
       <h1>Jose learning React</h1>
       <h3>Author:Joseph Mwangi</h3>
+      <img src = {insideImage} alt = "inside image"/>
     </div>
   );
 }
